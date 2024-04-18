2f532302aa9ad8c695ed8029579b822d693b7cbe
razondark <bryakovdima@yandex.ru>
2024-04-18 21:02:03 +0300

    init

---
+++ src/main/java/com/razondark/rtorgi/RTorgiApplication.java
@@ -0,0 +1,13 @@
+package com.razondark.rtorgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RTorgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RTorgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/razondark/rtorgi/RTorgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razondark.rtorgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RTorgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
