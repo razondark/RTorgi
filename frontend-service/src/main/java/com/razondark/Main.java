4305d2242846ff9db86a94339c816935d92bd4ee
razondark <bryakovdima@yandex.ru>
2024-04-18 21:15:37 +0300

    add base settings

---
+++ frontend-service/src/main/java/com/razondark/Main.java
@@ -0,0 +1,11 @@
+package com.razondark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
